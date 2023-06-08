108ba352b42f0a2d2a859fdfb4bba53cfb456a0e
PokIsemaine <851892190@qq.com>
2023-06-08 23:13:09 +0800

    init commit

---
+++ fib.go
@@ -0,0 +1,8 @@
+package Action_Demo
+
+func Fib(u uint) uint {
+	if u <= 1 {
+		return 1
+	}
+	return Fib(u-2) + Fib(u-1)
+}
---
+++ fib_test.go
@@ -0,0 +1,17 @@
+package Action_Demo
+
+import (
+	"testing"
+)
+
+func BenchmarkFib10(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var _ = Fib(10)
+	}
+}
+
+func BenchmarkFib20(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var _ = Fib(20)
+	}
+}
