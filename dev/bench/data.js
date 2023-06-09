e90a3b4845501a8ba86fe2ffe9a8a5b27c4e484f
github-action-benchmark <github@users.noreply.github.com>
2023-06-09 01:45:57 +0000

    add My Project Go Benchmark (go) benchmark result for 5b9d391540071a6949a17630f6e53713d0f91329

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1686275157409,
+  "repoUrl": "https://github.com/PokIsemaine/Action-Demo",
+  "entries": {
+    "My Project Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "851892190@qq.com",
+            "name": "PokIsemaine",
+            "username": "PokIsemaine"
+          },
+          "committer": {
+            "email": "851892190@qq.com",
+            "name": "PokIsemaine",
+            "username": "PokIsemaine"
+          },
+          "distinct": true,
+          "id": "5b9d391540071a6949a17630f6e53713d0f91329",
+          "message": "更新 GitHub Action",
+          "timestamp": "2023-06-09T09:45:29+08:00",
+          "tree_id": "4a3d25b2f7d0bc14c821f82edf48a9f219c09250",
+          "url": "https://github.com/PokIsemaine/Action-Demo/commit/5b9d391540071a6949a17630f6e53713d0f91329"
+        },
+        "date": 1686275156957,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFib10",
+            "value": 401,
+            "unit": "ns/op",
+            "extra": "3000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkFib20",
+            "value": 49353,
+            "unit": "ns/op",
+            "extra": "30000 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
